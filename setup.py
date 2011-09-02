c12e748bedf2a9b1df5726ea4bd00674ac800494
Adam Wagner <awagner83@gmail.com>
2011-09-02 14:17:51 -0400

    bumping version for new bugfix release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='doctools',
-    version='0.2.1',
+    version='0.2.2',
     description='docblock manipulation utilities',
     long_description=open('README.rst').read(),
     py_modules=['doctools'],
